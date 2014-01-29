f55893d64a042e5d0ffa328a6da40ee69ea5a632
Patrick Huck <phuck@lbl.gov>
2014-01-28 22:01:56 -0800

    settings: add commits schema

--- settings.py
+++ settings.py
@@ -1,4 +1,37 @@
 SERVER_NAME = '127.0.0.1:5000'
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'user'
+MONGO_PASSWORD = 'user'
+MONGO_DBNAME = 'apitest'
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+schema = {
+  'project': {
+    'type': 'string',
+    'minlength': 3,
+    'maxlength': 20,
+    'required': True,
+  },
+  'message': {
+    'type': 'string',
+    'minlength': 5,
+    'required': True,
+  },
+  'datetime': {
+    'type': 'datetime',
+    'required': True,
+  },
+}
+
+commits = {
+  'cache_control': 'max-age=10,must-revalidate',
+  'cache_expires': 10,
+  'schema': schema
+}
+
 DOMAIN = {
-  'commits': {},
+  'commits': commits,
 }
