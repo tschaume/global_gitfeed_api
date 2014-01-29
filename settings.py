fea2cb70a05ca288c52f412fc5ac2dea88a25bb2
Patrick Huck <phuck@lbl.gov>
2014-01-28 23:48:15 -0800

    update api scheme

--- settings.py
+++ settings.py
@@ -2,36 +2,48 @@ SERVER_NAME = '127.0.0.1:5000'
 
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
-MONGO_USERNAME = 'user'
-MONGO_PASSWORD = 'user'
-MONGO_DBNAME = 'apitest'
+#MONGO_USERNAME = 'user'
+#MONGO_PASSWORD = 'user'
+#MONGO_DBNAME = 'apitest'
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
-schema = {
-  'project': {
-    'type': 'string',
-    'minlength': 3,
-    'maxlength': 20,
-    'required': True,
-  },
-  'message': {
-    'type': 'string',
-    'minlength': 5,
-    'required': True,
-  },
-  'datetime': {
-    'type': 'datetime',
-    'required': True,
-  },
-}
-
-commits = {
-  'cache_control': 'max-age=10,must-revalidate',
-  'cache_expires': 10,
-  'schema': schema
-}
-
 DOMAIN = {
-  'commits': commits,
+  'commits': {
+    'schema': {
+      'project': {
+        'type': 'objectid',
+        'required': True,
+        'data_relation': {
+          'resource': 'projects',
+          'field': '_id',
+          'embeddable': True
+        }
+      },
+      'message': {
+        'type': 'string',
+        'minlength': 5,
+        'required': True,
+      },
+      'datetime': {
+        'type': 'datetime',
+        'required': True,
+      },
+    }
+  },
+  'projects': {
+    'additional_lookup': {
+      'url': 'regex("[\w]+")',
+      'field': 'name'
+    },
+    'schema': {
+      'name': {
+        'type': 'string',
+        'minlength': 3,
+        'maxlength': 20,
+        'required': True,
+        'unique': True,
+      },
+    }
+  }
 }
