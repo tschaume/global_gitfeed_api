17037f2bb9a75a4f6c483b479a1ec99af1d4c944
Patrick Huck <phuck@lbl.gov>
2014-01-30 15:56:18 -0500

    add eve_docs

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+import os
 from eve import Eve
-app = Eve()
+from flask.ext.bootstrap import Bootstrap
+from eve_docs import eve_docs
 
 if __name__ == '__main__':
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app = Eve()
+  Bootstrap(app)
+  app.register_blueprint(eve_docs, url_prefix='/docs')
+  app.run(host='127.0.0.1', port=port, debug=True)
